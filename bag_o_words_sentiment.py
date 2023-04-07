ab296ce760052625cdd3232385aa8b11768a25b2
TBSDrJ <marcus.jaiclin@bishops.com>
2023-04-07 14:34:12 -0700

    Set up the vectorization workflow.

--- bag_o_words_sentiment.py
+++ bag_o_words_sentiment.py
@@ -1,8 +1,18 @@
 import random
+import tensorflow as tf
 import tensorflow.keras.layers as layers
 
+binary_vectorization = layers.TextVectorization(output_mode='binary')
 
-def datasetConfig(filename, batch=32, train_test_split=0.30):
+def vectorize_entry(sentence, label):
+    print(sentence, label)
+    sentence = tf.convert_to_tensor(sentence)
+    sentence = tf.expand_dims(sentence, -1)
+    label = tf.convert_to_tensor(label, dtype=tf.float32)
+    return binary_vectorization(sentence), label
+
+
+def datasetConfig(filename, batch=32, train_valid_split=0.30):
     # Set up movie reviews so it can be used as a bag of words
     with open(filename, 'r') as fin:
         lines = fin.readlines()
@@ -16,11 +26,30 @@ def datasetConfig(filename, batch=32, train_test_split=0.30):
     # Having hard time getting zip object to iterate as I thought
     #   it should, so I converted to list.
     data = list(zip(sentences, labels))
-    print("\n" * 3)
-    print("Sampling of input data:", end="\n\n")
-    for i in range(10):
-        # equivalent to labels[i], sentences[i]
-        print(f"{data[i][1]}\t{data[i][0]}")
-        
+    # # Show a sampling of the input data
+    # print("\n" * 3)
+    # print("Sampling of input data:", end="\n\n")
+    # for i in range(10):
+    #     # equivalent to labels[i], sentences[i]
+    #     print(f"{data[i][1]}\t{data[i][0]}")
+    # print()
+    # Set up train-test split
+    random.shuffle(data)
+    split_point = int(train_valid_split*(len(data)))
+    valid_data = data[:split_point]
+    train_data = data[split_point:]
+    # Set up vocabulary list using adapt().
+    # It is important to use *only* the training data for this.
+    train_text = [entry[0] for entry in train_data]
+    binary_vectorization.adapt(train_text)
+    # # Show results of vectorizing the data
+    # ex_sentence, ex_label = vectorize_entry(train_data[0][0], train_data[0][1])
+    # print(ex_sentence)
+    # print(ex_label)
+    # for i, entry in enumerate(ex_sentence.numpy()[0]):
+    #     if int(entry) == 1:
+    #         print(binary_vectorization.get_vocabulary()[i])
+    
+   
 
 dataset = datasetConfig('movieReviews.txt')
